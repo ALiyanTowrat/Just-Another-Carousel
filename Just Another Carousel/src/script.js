a5c71c44a4a49799817a2c59e05bc686050142d0
AliyanTowrat <110545706+ALiyanTowrat@users.noreply.github.com>
2024-01-17 00:21:48 +0500

    first commit

---
+++ Just Another Carousel/src/script.js
@@ -0,0 +1,43 @@
+/*
+inspiration
+https://cz.pinterest.com/pin/830703093790696716/
+*/
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  spaceBetween: 30,
+  centeredSlides: false,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: false
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  breakpoints: {
+    460: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 3
+    },
+    1600: {
+      slidesPerView: 3.6
+    }
+  }
+});
